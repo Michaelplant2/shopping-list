209d26c8eb5d36984a7d0cf77418f32bd024188b
Michael Plantamura <105463635+Michaelplant2@users.noreply.github.com>
2023-06-07 10:48:31 -0400

    Update shopping_list.py

--- shopping_list.py
+++ shopping_list.py
@@ -1,9 +1,22 @@
+shopping_list = []
+
+
 def show_help():
     print("What should we get at the store")
     print("""
     Enter "DONE" to end list
+    Enter "SHOW" to show your list
     Enter "HELP" to show help
     """)
+
+def add_to_list(item):
+    shopping_list.append(item)
+    print("You have {} items in your list".format(len(shopping_list)))
+
+def show_list():
+    print("Your list: ")
+    for item in shopping_list:
+        print("* {}".format(item))
 
 show_help()
 while True:
@@ -14,3 +27,8 @@ while True:
     elif new_item == "HELP":
         show_help()
         continue
+    elif new_item == "SHOW":
+        show_list()
+        continue
+
+    add_to_list(new_item)
